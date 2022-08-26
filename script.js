51c50bf46dc0b31044b96e419cfaa5552f372035
annazhutava <anna.zhutava@ecutbildning.se>
2022-08-26 18:03:22 +0200

    fetch + show movies

---
+++ script.js
@@ -0,0 +1,56 @@
+// TMBD
+
+const API_KEY = 'api_key=82b2f1aebd1488622056f64bf0d6d9b3';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+const main = document.getElementById('main');
+
+getMovies(API_URL);
+
+function getMovies(url) {
+
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data);
+        showMovies(data.results);
+    })
+}
+
+function showMovies(data){
+
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML =  `
+        <img src= "${IMG_URL + poster_path}" alt="${title}">
+            
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getColor(vote_average)}">${vote_average}</span>
+        </div>
+
+        <div class="overview">
+            <h3>Overview</h3>
+            ${overview}
+        </div>
+        `
+
+        main.appendChild(movieEl);
+    })
+}
+
+function getColor(vote) {
+    if(vote >= 8){
+        return 'green'
+    }
+    else if(vote >= 5){
+        return 'orange'
+    }
+    else{
+        return 'red'
+    }
+}
